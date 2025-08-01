ff25ae0651566ec629331e4b787e2f87d1d6a6e7
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-07-31 20:21:19 -0700

    styled

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 // YOUR JS CODE HERE
+async function getData(url){
+    try{
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+    }catch(err){
+        console.log(err)
+    }
+}
+
+function writeData(arr){
+    const container = document.querySelector('.container')
+
+    const update_date = new Date(arr.current.time)
+    const temp = arr.current.temperature_2m
+
+    container.innerHTML = `
+    <h2>My Weather</h2>
+    <h3 class="temperature ${temp>20? 'hot': temp<10?'cool':''}">
+    ${temp} <span class="temp-unit">${arr.current_units.temperature_2m}<span>
+    </h3>
+    <h3 class="wind-speed">Wind Speed: ${arr.current.wind_speed_10m} ${arr.current_units.wind_speed_10m}</h3>
+    <h3 class="location">${arr.timezone}</h3>
+    <h3 class="updated-date">Last updated: ${update_date.toLocaleString()}</h3>
+    `
+}
+
+function updateDate(arr){
+    const container = document.querySelector('.container')
+    container.innerHTML= 'Loading...'
+
+    writeData(arr)
+}
+
+document.addEventListener('DOMContentLoaded', async function (){
+    const data = await getData("https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1")
+    writeData(data)
+})
+
+document.querySelector('.btn-update').addEventListener('click', async function(){
+    const data = await getData("https://api.open-meteo.com/v1/forecast?latitude=49.2497&longitude=-123.1193&current=temperature_2m,is_day,rain,showers,wind_speed_10m&timezone=auto&forecast_days=1")
+    updateDate(data)
+})
+
